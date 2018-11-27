8adc43e28c2bd57cf115a3fedffba9f08260e369
Jayshon <realj2134@gamil.com>
2018-11-27 15:38:35 -0500

    Navbar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';

361c05a57ea4d54cd9b95f1ae481fbc489b9c538
Connor Mellencamp <connorsmac@Connors-MacBook-Pro.local>
2018-11-27 15:32:00 -0500

    a lot of changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services.service.ts
+++ src/app/services.service.ts
@@ -7,7 +7,8 @@ import { Auth } from './models/auth'
 const endpoint = 'https://efa-gardenapp-backend.herokuapp.com/api/product'
 const httpOptions = {
   headers: new HttpHeaders({
-    'Content-Type': 'application/json'
+    'Content-Type': 'application/json',
+    'Authorization': JSON.parse(localStorage.getItem('token'))
   })
 };
 
@@ -23,39 +24,33 @@ export class ServicesService {
     return body || {};
   }
 
-
-
   getProducts(): Observable<any> {
-    return this.http.get(endpoint + 'products').pipe(
+    return this.http.get(endpoint).pipe(
       map(this.extractData));
   }
+
   deleteProduct(id): Observable<any> {
-    return this.http.delete<any>(endpoint + 'products/' + id, httpOptions).pipe(
+    return this.http.delete<any>(endpoint + '/' + id, httpOptions).pipe(
       tap(_ => console.log(`deleted product id=${id}`)),
       catchError(this.handleError<any>('deleteProduct'))
     );
   }
 
+  login(email:string, password:string){
+    return this.http.post<any>('https://efa-gardenapp-backend.herokuapp.com/api/auth/login', {email: email, password: password})
+      .pipe(map(user => {
+        if (user) {
+          localStorage.setItem('token', JSON.stringify(user.token));
+        }
+        return user;
+      }));
+  }
+
   private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
-
-
       console.error(error);
-
-      // TODO: better job of transforming error for user consumption
       console.log(`${operation} failed: ${error.message}`);
-
-      // Let the app keep running by returning an empty result.
       return of(result as T);
     };
   }
 }
-
-
-
- //delete
-
- //get products
-
- //login/auth
- //login/auth
